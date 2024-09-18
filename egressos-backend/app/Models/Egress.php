8ced7e6f339403353636241de8ff3fe3ca0aec1c
FIXER3600 <guilhermecomercial51@gmail.com>
2024-09-18 11:28:27 -0300

    feat: criando model de egresso

---
+++ egressos-backend/app/Models/Egress.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Egress extends Model
+{
+    use HasFactory;
+        /**
+     * Os atributos que são atribuíveis em massa.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'cpf',
+        'phone',
+        'birthdate',
+        'email',
+        'status'
+    ];
+        /**
+     * Os atributos que devem ser mutados para tipos nativos.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'birthdate' => 'datetime',
+    ];
+}
--- egressos-backend/database/factories/UserFactory.php
+++ egressos-backend/database/factories/UserFactory.php
@@ -22,7 +22,6 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'tipo_conta' => enum('tipo_conta', ['admin', 'user'])->default('user'),
-            'remember_token' => Str::random(10),
         ];
     }
 
