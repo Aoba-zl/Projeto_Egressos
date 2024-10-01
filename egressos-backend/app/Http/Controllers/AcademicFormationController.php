ad2464a9dbaa61183fc6264de35c604c1acbf226
FIXER3600 <guilhermecomercial51@gmail.com>
2024-10-01 11:52:12 -0300

    feat: adicionando formação academica

--- egressos-backend/app/Http/Controllers/AcademicFormationController.php
+++ egressos-backend/app/Http/Controllers/AcademicFormationController.php
@@ -20,6 +20,25 @@ class AcademicFormationController extends Controller
     public function store(Request $request)
     {
         //
+        $request->validate([
+            'id_profile' => 'required|exists:platforms,id',
+            'id_institution' => 'required|exists:egresses,id',
+            'id_course' => 'required|exists:course,id',
+            'begin_year' => 'required|integer',
+            'end_year' => 'integer',
+            'period' => 'required|string|max:255',
+        ]);
+
+        $acadFormation = AcademicFormation::create([
+            'id_profile' => $request->id_profile,
+            'id_institution' => $request->id_institution,
+            'id_course' => $request->id_course,
+            'begin_year' => $request->begin_year,
+            'end_year' => $request->end_year,
+            'period' => $request->period,
+        ]);
+
+        return response()->json($acadFormation);
     }
 
     /**
