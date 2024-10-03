7ed2f8eae0c3268a4a1732ac82a607aa58f49414
DeboraCristina <deboracristinaproficional1@gmail.com>
2024-10-03 10:55:14 -0300

    Update: Add TODOS; criação de contatos, egressos e usuarios; Gambiarra de Validação pra não criar dados atoa.

--- egressos-backend/app/Http/Controllers/EgressController.php
+++ egressos-backend/app/Http/Controllers/EgressController.php
@@ -2,10 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\AcademicFormation;
-use App\Models\Contact;
 use App\Models\Egress;
-use App\Models\ProfessionalProfile;
 use App\Http\Controllers\UserController;
 use App\Http\Requests\StoreEgressRequest;
 use App\Http\Requests\StoreUserRequest;
@@ -34,18 +31,22 @@ class EgressController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreEgressRequest $request)
+    public function store(Request $request) // TODO: StoreEgressRequest valida, mas é preciso resolver as validações.
     {
         $userController = new UserController;
         $contactController = new ContactController;
         $academicFormationController = new AcademicFormationController;
         $professionalProfileController = new ProfessionalProfileController;
 
+        $this->validateRequest($request);
 
-        $user = $this->validateUser($userController->store(
+
+        // TODO: Validar se realmente criou
+        $user = $userController->store(
             new StoreUserRequest($request->only(['name', 'email', 'password', 'type_account']))
-        ));
+        )->original['user'];
 
+        // TODO: Validar se realmente criou
         $egress = Egress::create([
             'user_id' => $user->id,
             'cpf' => $request->input('cpf'),
@@ -54,17 +55,19 @@ class EgressController extends Controller
             'status' => "0"
         ]);
 
-        /*
-        foreach ($request->contats as $contactData)
-            // TODO: Validar
+        foreach ($request->contacts as $contactData)
+            // TODO: Validar se realmente criou
             $contact = $contactController->store(
-                new Request($contactData)
+                new Request([
+                    'id_profile' => $egress->id,
+                    'id_platform' => $contactData['platform'],
+                    'contact' => $contactData['contact'],
+                ])
             );
-        */
 
         /*
         foreach ($request->academic_formation as $academicFormationData)
-            // TODO: Validar
+            // TODO: Validar se realmente criou
             $academicFormation = $academicFormationController->store(
                 new Request($academicFormationData)
             );
@@ -72,7 +75,7 @@ class EgressController extends Controller
 
         /*
         foreach ($request->professional_profile as $professionalProfileData)
-            // TODO: Validar
+            // TODO: Validar se realmente criou
             $professionalProfile = $professionalProfileController->store(
                 new Request($professionalProfileData)
             );
@@ -81,11 +84,29 @@ class EgressController extends Controller
         return response()->json($egress);
     }
 
-    private function validateUser(JsonResponse $userJson)
+    /*
+     * Validate that the request meet all classes
+     * // TODO: Gambiarra
+     */
+    private function validateRequest(Request $request)
     {
-        $user = $userJson->original['user'];
-        // TODO: Validar
-        return $user;
+        $request->validate([
+            'name' => ['required', 'string', 'max:255', 'regex:/^[a-zA-ZÀ-ú\s]+$/'],
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => ['required', 'string', 'min:8', 'regex:/[A-Z]/', 'regex:/[0-9]/', 'regex:/[@$!%*#?&]/'],
+            'type_account' => 'required|in:0,1,2',
+        ]);
+
+        $request->validate([
+            'cpf' => ['required', 'cpf', 'digits:11', 'unique:egresses,cpf'],
+            'phone' => ['required', 'digits:11'],
+            'birthdate' => ['required', 'date', 'before:01-01-' . now()->year, 'after:01/01/1900']
+        ]);
+
+        $request->validate([
+            'contacts.*.platform' => 'required|exists:platforms,id',
+            'contacts.*.contact' => 'required|string|unique:contacts,contact'
+        ]);
     }
 
     /**
