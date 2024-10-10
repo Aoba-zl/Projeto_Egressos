ce356a9b7f0dadbe861082551d4121216f9e6b51
Vinicius Oliveira <vinilimadeoliveira@hotmail.com>
2024-10-10 16:25:13 -0300

    feat: cria funcao que gera hash para as senhas

--- frontend/egressos/js/globals.js
+++ frontend/egressos/js/globals.js
@@ -1,1 +1,15 @@
-var serverUrl = "http://localhost:8000/api/";
+const serverUrl = "http://localhost:8000/api/";
+
+// consultar mdn docs
+async function generateHash(value) {
+    value+="portalDeEgressosFatecZlSalt";
+    const encoder = new TextEncoder();
+    const data = encoder.encode(value);
+    const hashBuffer = await window.crypto.subtle.digest("SHA-256", data);
+
+    const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
+    const hashHex = hashArray
+      .map((b) => b.toString(16).padStart(2, "0"))
+      .join(""); // convert bytes to hex string
+    return hashHex;
+}
