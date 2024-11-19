e8c5c5be4c6c277c846144a0a739507af7c3b414
Vinicius Oliveira <vinilimadeoliveira@hotmail.com>
2024-11-19 15:24:36 -0300

    fix: removendo comentario do get em assessment

--- egressos-backend/routes/api.php
+++ egressos-backend/routes/api.php
@@ -110,7 +110,7 @@ Route::put('/user/{id}', [UserController::class, 'update']);
 
 //-----------------------------------------------------------------
 //-------------------ASSESSMENT------------------------------------
-//Route::get('assessment/{id}',[AssessmentController::class,'show']);
+Route::get('assessment/{id}',[AssessmentController::class,'show']);
 Route::post('saveAssessment',[AssessmentController::class,'store']);
 
 //----------------------- ADDRESSES -------------------------------
