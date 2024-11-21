61c3d98a2b5502a312ab00e76140d68f30bc3eb2
DeboraCristina <deboracristinaproficional1@gmail.com>
2024-11-21 13:17:37 -0300

    Add: Front Redefinir senha requisita código de validação

--- frontend/egressos/js/redefinirSenha_script.js
+++ frontend/egressos/js/redefinirSenha_script.js
@@ -6,5 +6,59 @@ window.onload = function () {
 }
 
 function init(){
-     
+     //
 }
+
+
+async function reset_password() 
+{
+    let email = document.getElementById('txtEmail').value
+
+    if (!email)
+        showAlert('Digite um e-mail', 'alert-danger')
+
+    try
+    {
+        let endpoint = serverUrl + "resetpasswd";
+
+        const response = await fetch(endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email: email }),
+        })
+
+        const data = await response.json();
+
+        if (response.ok)
+        {
+            showAlert('E-mail com código de validação enviado', 'alert-success')
+            alert('E-mail com código de validação enviado')
+            window.location.href = "./index.html"
+        }
+        else
+            showAlert("Erro ao enviar")
+
+    }
+    catch (error) {
+        console.log(error)
+    }
+    
+}
+
+function showAlert(message, alertType) {
+    const alertPlaceholder = document.createElement('div');
+    alertPlaceholder.className = `alert ${alertType} mt-3`;
+    alertPlaceholder.textContent = message;
+    document.querySelector('main').prepend(alertPlaceholder);
+
+    // Remove alert after 5 seconds
+    setTimeout(() => {
+        alertPlaceholder.remove();
+    }, 5000);
+}
+
+
+var btn_redefinir_senha = document.getElementById('btnResetPasswd').addEventListener('click', reset_password)
+
